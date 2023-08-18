2a5f73903e68cda9773d2ce8d6f0185ca68008aa
kgmikhdad <96974190+kgmikhdad@users.noreply.github.com>
2023-08-18 07:50:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import yfinance as yf
-import matplotlib.pyplot as plt
 
 def get_data(stock):
     try:
@@ -45,18 +44,6 @@ def get_data(stock):
         st.error(f'An error occurred: {e}')
         return {}, {}
 
-def plot_stock_chart(stock):
-    ticker = yf.Ticker(stock)
-    hist = ticker.history(period="1y")
-
-    # Plotting the stock's closing prices
-    plt.figure(figsize=(10, 6))
-    hist['Close'].plot(title=f'{stock} Stock Price Over the Last Year')
-    plt.xlabel('Date')
-    plt.ylabel('Closing Price')
-    plt.grid(True)
-    st.pyplot(plt)
-
 def main():
     st.title("Stock Fundamental and Technical Indicators")
 
@@ -70,9 +57,6 @@ def main():
 
         st.write("## Technical Indicators")
         st.write(technicals)
-
-        st.write("## Stock Price Chart")
-        plot_stock_chart(stock)
 
 if __name__ == "__main__":
     main()
