27bdb0d6b773c2ce53e27ecd15fb7bad7121bf4b
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-18 17:19:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,48 @@
 import streamlit as st
 import yfinance as yf
-import plotly.graph_objects as go
 
 def get_data(stock):
-    # ... [same as your code]
+    try:
+        ticker = yf.Ticker(stock)
+
+        # Check if ticker.info is a valid dictionary
+        if not ticker.info or not isinstance(ticker.info, dict):
+            st.error(f'Failed to fetch data for {stock}. Please try another stock ticker.')
+            return {}, {}
+
+        # Fundamental Indicators with error handling for missing keys
+        fundamentals = {
+            'Market Cap': ticker.info.get('marketCap', 'N/A'),
+            'Forward P/E': ticker.info.get('forwardPE', 'N/A'),
+            'Dividend Yield': ticker.info.get('dividendYield', 'N/A'),
+            'Price to Book (P/B)': ticker.info.get('priceToBook', 'N/A'),
+            'Earnings Per Share (EPS)': ticker.info.get('trailingEps', 'N/A'),
+            'Revenue': ticker.info.get('revenue', 'N/A'),
+            'Profit Margin': ticker.info.get('profitMargins', 'N/A'),
+            'Book Value': ticker.info.get('bookValue', 'N/A'),
+            'Beta': ticker.info.get('beta', 'N/A'),
+            '52 Week High': ticker.info.get('fiftyTwoWeekHigh', 'N/A')
+        }
+
+        # Technical Indicators (using simple calculations)
+        hist = ticker.history(period="1y")
+        sma50 = hist['Close'].rolling(window=50).mean().iloc[-1]
+        sma200 = hist['Close'].rolling(window=200).mean().iloc[-1]
+
+        technicals = {
+            '50 Day SMA': sma50,
+            '200 Day SMA': sma200,
+            'Current Price': hist['Close'].iloc[-1],
+            '52 Week Low': hist['Low'].min(),
+            '52 Week High': hist['High'].max(),
+            '1 Year Change %': ((hist['Close'].iloc[-1] - hist['Close'].iloc[0]) / hist['Close'].iloc[0]) * 100
+        }
+
+        return fundamentals, technicals
+
+    except Exception as e:
+        st.error(f'An error occurred: {e}')
+        return {}, {}
 
 def main():
     st.title("Stock Fundamental and Technical Indicators")
@@ -13,39 +52,11 @@ def main():
     if stock:
         fundamentals, technicals = get_data(stock)
 
-        # Using Bootstrap styling for cards
-        st.markdown("""
-        <style>
-            .card {
-                border: 1px solid #e1e4e8;
-                border-radius: 6px;
-                padding: 16px;
-                margin: 20px 0;
-            }
-        </style>
-        """, unsafe_allow_html=True)
-
-        # Displaying Fundamental Indicators in a card
-        st.markdown("<div class='card'>", unsafe_allow_html=True)
         st.write("## Fundamental Indicators")
-        for key, value in fundamentals.items():
-            st.write(f"**{key}**: {value}")
-        st.markdown("</div>", unsafe_allow_html=True)
+        st.write(fundamentals)
 
-        # Displaying Technical Indicators in a card
-        st.markdown("<div class='card'>", unsafe_allow_html=True)
         st.write("## Technical Indicators")
-        for key, value in technicals.items():
-            st.write(f"**{key}**: {value}")
-        st.markdown("</div>", unsafe_allow_html=True)
-
-        # Plotting some of the technical indicators
-        fig = go.Figure()
-        fig.add_trace(go.Scatter(y=[technicals['50 Day SMA'], technicals['200 Day SMA'], technicals['Current Price']],
-                                 x=['50 Day SMA', '200 Day SMA', 'Current Price'],
-                                 mode='lines+markers'))
-        st.plotly_chart(fig)
+        st.write(technicals)
 
 if __name__ == "__main__":
     main()
- 
