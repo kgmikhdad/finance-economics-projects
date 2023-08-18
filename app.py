f58fa201bb4f2ff30326037e801934b4f65833e2
kgmikhdad <96974190+kgmikhdad@users.noreply.github.com>
2023-08-18 07:06:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-a = int(input("Enter any number:"))
-b = int(input("Enter any number:"))
-c=a+b
-print(c)
+# app.py
+import streamlit as st
 
+def main():
+    st.title("Simple Streamlit App")
+    st.write("Welcome to this simple Streamlit app!")
+    
+    user_input = st.text_input("Enter your name:", "John Doe")
+    st.write(f"Hello, {user_input}!")
+
+if __name__ == "__main__":
+    main()
