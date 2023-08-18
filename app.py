4c4e15325f886090bbb56ca998ca71080f6bd975
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-18 17:00:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,62 +1,39 @@
 import streamlit as st
 import yfinance as yf
+import pandas as pd
 
 def get_data(stock):
-    try:
-        ticker = yf.Ticker(stock)
-
-        # Check if ticker.info is a valid dictionary
-        if not ticker.info or not isinstance(ticker.info, dict):
-            st.error(f'Failed to fetch data for {stock}. Please try another stock ticker.')
-            return {}, {}
-
-        # Fundamental Indicators with error handling for missing keys
-        fundamentals = {
-            'Market Cap': ticker.info.get('marketCap', 'N/A'),
-            'Forward P/E': ticker.info.get('forwardPE', 'N/A'),
-            'Dividend Yield': ticker.info.get('dividendYield', 'N/A'),
-            'Price to Book (P/B)': ticker.info.get('priceToBook', 'N/A'),
-            'Earnings Per Share (EPS)': ticker.info.get('trailingEps', 'N/A'),
-            'Revenue': ticker.info.get('revenue', 'N/A'),
-            'Profit Margin': ticker.info.get('profitMargins', 'N/A'),
-            'Book Value': ticker.info.get('bookValue', 'N/A'),
-            'Beta': ticker.info.get('beta', 'N/A'),
-            '52 Week High': ticker.info.get('fiftyTwoWeekHigh', 'N/A')
-        }
-
-        # Technical Indicators (using simple calculations)
-        hist = ticker.history(period="1y")
-        sma50 = hist['Close'].rolling(window=50).mean().iloc[-1]
-        sma200 = hist['Close'].rolling(window=200).mean().iloc[-1]
-
-        technicals = {
-            '50 Day SMA': sma50,
-            '200 Day SMA': sma200,
-            'Current Price': hist['Close'].iloc[-1],
-            '52 Week Low': hist['Low'].min(),
-            '52 Week High': hist['High'].max(),
-            '1 Year Change %': ((hist['Close'].iloc[-1] - hist['Close'].iloc[0]) / hist['Close'].iloc[0]) * 100
-        }
-
-        return fundamentals, technicals
-
-    except Exception as e:
-        st.error(f'An error occurred: {e}')
-        return {}, {}
+    # ... [Keep the existing get_data function as is]
 
 def main():
-    st.title("Stock Fundamental and Technical Indicators")
+    st.title("Stock Fundamental and Technical Indicators Dashboard")
 
-    stock = st.text_input("Enter stock ticker (e.g. AAPL):")
+    # Sidebar for user input
+    st.sidebar.header("Input Stock Ticker")
+    stock = st.sidebar.text_input("Enter stock ticker (e.g. AAPL):")
 
     if stock:
         fundamentals, technicals = get_data(stock)
 
-        st.write("## Fundamental Indicators")
-        st.write(fundamentals)
+        # Display stock name and logo
+        ticker = yf.Ticker(stock)
+        st.image(ticker.info['logo_url'], width=100)
+        st.write(f"## {ticker.info['longName']}")
 
-        st.write("## Technical Indicators")
-        st.write(technicals)
+        # Display Fundamental Indicators
+        st.write("### Fundamental Indicators")
+        fundamentals_df = pd.DataFrame.from_dict(fundamentals, orient='index', columns=['Value'])
+        st.table(fundamentals_df)
+
+        # Display Technical Indicators
+        st.write("### Technical Indicators")
+        technicals_df = pd.DataFrame.from_dict(technicals, orient='index', columns=['Value'])
+        st.table(technicals_df)
+
+        # Display stock price chart
+        st.write("### Stock Price Chart")
+        hist = yf.Ticker(stock).history(period="1y")
+        st.line_chart(hist['Close'])
 
 if __name__ == "__main__":
     main()
