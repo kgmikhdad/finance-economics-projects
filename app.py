198f23b0e9c7f843aa2626428a614a1a4c50900e
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-20 10:33:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,40 @@
 import streamlit as st
-import yfinance as yf
-import plotly.graph_objects as go
-from datetime import datetime, timedelta
 
-def plot_stock_data(stock_name, duration):
-    # Current date
-    today = datetime.today().strftime('%Y-%m-%d')
-    
-    # Calculate start date based on duration
-    if duration == '1d':
-        start_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
-    elif duration == '1w':
-        start_date = (datetime.today() - timedelta(weeks=1)).strftime('%Y-%m-%d')
-    elif duration == '1m':
-        start_date = (datetime.today() - timedelta(days=30)).strftime('%Y-%m-%d')
-    elif duration == '3m':
-        start_date = (datetime.today() - timedelta(days=90)).strftime('%Y-%m-%d')
-    elif duration == '1y':
-        start_date = (datetime.today() - timedelta(days=365)).strftime('%Y-%m-%d')
-    else:
-        start_date = "2020-01-01"  # Default start date
-    
-    # Fetch data for the stock
-    df = yf.download(stock_name, start=start_date, end=today)
+def get_input(prompt, min_val, max_val):
+    try:
+        value = st.number_input(prompt, min_value=min_val, max_value=max_val)
+        if min_val <= value <= max_val:
+            return value
+        else:
+            st.warning(f"Please enter a value between {min_val} and {max_val}.")
+            return get_input(prompt, min_val, max_val)
+    except ValueError:
+        st.warning("Please enter a valid number.")
+        return get_input(prompt, min_val, max_val)
 
-    # Create an interactive line plot
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=df.index, y=df['Close'], name=f'Closing Price of {stock_name}'))
+def calculate_cibil_score():
+    st.title("CIBIL Score Calculator")
+    st.write("Enter the following details to calculate your CIBIL score:")
 
-    # Add interactivity and adjustability
-    fig.update_layout(
-        title=f'Price Plot for {stock_name}',
-        xaxis_title='Date',
-        yaxis_title='Price',
-        hovermode='x'
-    )
+    # Get user input with specified ranges
+    payment_history = get_input("Percentage of timely payments (0-100): ", 0, 100)
+    credit_utilization = get_input("Credit utilization ratio (0-100): ", 0, 100)
+    length_of_credit_history = get_input("Length of credit history in years (e.g., 0-50): ", 0, 50)
+    types_of_credit = get_input("Number of types of credit (e.g., 1-5): ", 1, 5)
+    new_credit = get_input("Number of new credits opened recently (e.g., 0-10): ", 0, 10)
 
-    # Display the interactive graph
-    st.plotly_chart(fig)
+    # Calculate score
+    score = 0
+    score += payment_history * 0.35
+    score += (100 - credit_utilization) * 0.30  # Lower utilization is better
+    score += (length_of_credit_history / 10) * 15  # Assuming 10+ years is best
+    score += types_of_credit * 2  # Assuming 5 types is best
+    score -= new_credit * 2  # Assuming 0 new credit is best
 
-def main():
-    st.title("Stock Price Plot")
-    stock_name = st.text_input("Enter the stock code:", value="AAPL").strip().upper()
-    duration = st.selectbox("Choose the duration:", ['1d', '1w', '1m', '3m', '1y'])
-    
-    if st.button("Plot"):
-        plot_stock_data(stock_name, duration)
+    # Ensure score boundaries
+    score = max(300, min(900, score * 10))
+
+    st.success(f"Your estimated CIBIL score is: {score}")
 
 if __name__ == "__main__":
-    main()
+    calculate_cibil_score()
