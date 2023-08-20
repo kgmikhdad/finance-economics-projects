d9cc13b7c99a70e0a012d33153abcd39837cb3a2
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-20 09:54:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,51 @@
-# ... [previous code]
+import streamlit as st
+import yfinance as yf
+import plotly.graph_objects as go
+from datetime import datetime, timedelta
+
+def plot_stock_data(stock_name, duration):
+    # Current date
+    today = datetime.today().strftime('%Y-%m-%d')
+    
+    # Calculate start date based on duration
+    if duration == '1d':
+        start_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+    elif duration == '1w':
+        start_date = (datetime.today() - timedelta(weeks=1)).strftime('%Y-%m-%d')
+    elif duration == '1m':
+        start_date = (datetime.today() - timedelta(days=30)).strftime('%Y-%m-%d')
+    elif duration == '3m':
+        start_date = (datetime.today() - timedelta(days=90)).strftime('%Y-%m-%d')
+    elif duration == '1y':
+        start_date = (datetime.today() - timedelta(days=365)).strftime('%Y-%m-%d')
+    else:
+        start_date = "2020-01-01"  # Default start date
+    
+    # Fetch data for the stock
+    df = yf.download(stock_name, start=start_date, end=today)
+
+    # Create an interactive line plot
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=df.index, y=df['Close'], name=f'Closing Price of {stock_name}'))
+
+    # Add interactivity and adjustability
+    fig.update_layout(
+        title=f'Price Plot for {stock_name}',
+        xaxis_title='Date',
+        yaxis_title='Price',
+        hovermode='x'
+    )
+
+    # Display the interactive graph
+    st.plotly_chart(fig)
 
 def main():
-    st.markdown(
-    """
-    <style>
-        .times-font {
-            font-family: "Times New Roman", Times, serif;
-            font-size: 30px;
-            text-align: center;
-            font-weight: bold;
-        }
-    </style>
-    <div class="times-font">Analyse a stock</div>
-    """, 
-    unsafe_allow_html=True)
-
-    stock = st.text_input("Enter stock ticker (e.g. AAPL):")
-
-    if stock:
-        fundamentals, technicals = get_data(stock)
-
-        # Create two columns to display data side by side
-        col1, col2 = st.columns(2)
-
-        with col1:
-            st.write("## Fundamental Indicators")
-            for key, value in fundamentals.items():
-                # Display each fundamental as a dropdown using native HTML details/summary tags
-                st.markdown(f"""
-                <details>
-                    <summary>{key}</summary>
-                    <p>{value}</p>
-                </details>
-                """, unsafe_allow_html=True)
-        
-        with col2:
-            st.write("## Technical Indicators")
-            for key, value in technicals.items():
-                # Display each technical indicator as a dropdown using native HTML details/summary tags
-                st.markdown(f"""
-                <details>
-                    <summary>{key}</summary>
-                    <p>{value}</p>
-                </details>
-                """, unsafe_allow_html=True)
+    st.title("Stock Price Plot")
+    stock_name = st.text_input("Enter the stock code:", value="AAPL").strip().upper()
+    duration = st.selectbox("Choose the duration:", ['1d', '1w', '1m', '3m', '1y'])
+    
+    if st.button("Plot"):
+        plot_stock_data(stock_name, duration)
 
 if __name__ == "__main__":
     main()
