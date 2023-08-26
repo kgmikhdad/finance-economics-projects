f088a1f6dc93de330b2f6a3cbe72785b93e001fc
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-26 19:00:58 +0530

    Update stock_app.py

--- stock_health/stock_app.py
+++ stock_health/stock_app.py
@@ -3,74 +3,72 @@ import yfinance as yf
 import pandas as pd
 import numpy as np
 
-# Fetch stock data
+# Fetch stock data and info
 def get_stock_data(ticker):
-    data = yf.Ticker(ticker)
-    return data.history(period="1y")
+    stock = yf.Ticker(ticker)
+    data = stock.history(period="1y")
+    info = stock.info
+    return data, info
 
 # Compute technical indicators
 def compute_technical_indicators(data):
-    # Moving Averages
-    data['MA50'] = data['Close'].rolling(50).mean()
-    data['MA200'] = data['Close'].rolling(200).mean()
-    
-    # RSI
-    delta = data['Close'].diff(1)
-    gain = (delta.where(delta > 0, 0)).fillna(0)
-    loss = (-delta.where(delta < 0, 0)).fillna(0)
-    avg_gain = gain.rolling(14).mean()
-    avg_loss = loss.rolling(14).mean()
-    rs = avg_gain / avg_loss
-    data['RSI'] = 100 - (100 / (1 + rs))
-    
-    # MACD
-    data['MACD'] = data['Close'].ewm(span=12, adjust=False).mean() - data['Close'].ewm(span=26, adjust=False).mean()
-    data['MACD_signal'] = data['MACD'].ewm(span=9, adjust=False).mean()
-    
-    # Bollinger Bands
-    data['BB_upper'] = data['Close'].rolling(20).mean() + 2*data['Close'].rolling(20).std()
-    data['BB_lower'] = data['Close'].rolling(20).mean() - 2*data['Close'].rolling(20).std()
-    
-    # Stochastic Oscillator
-    high14 = data['High'].rolling(14).max()
-    low14 = data['Low'].rolling(14).min()
-    data['%K'] = (data['Close'] - low14) / (high14 - low14) * 100
-    data['%D'] = data['%K'].rolling(3).mean()
-    
-    return data
+    # ... [Same as previous code for technical indicators] ...
 
 # Recommendation based on indicators
-def get_recommendation(data):
+def get_recommendation(fundamentals, technicals):
     # Basic logic for recommendation
-    if data['Close'].iloc[-1] > data['MA50'].iloc[-1] and data['MACD'].iloc[-1] > data['MACD_signal'].iloc[-1]:
+    # This is a very rudimentary recommendation system. In a real-world scenario, more sophisticated algorithms would be used.
+    buy_signals = 0
+    sell_signals = 0
+
+    # Fundamental checks
+    if fundamentals["P/E"] < 20:  # P/E ratio less than 20 is generally considered good
+        buy_signals += 1
+    else:
+        sell_signals += 1
+
+    if fundamentals["P/B"] < 1:  # P/B ratio less than 1 might indicate the stock is undervalued
+        buy_signals += 1
+    else:
+        sell_signals += 1
+
+    # Technical checks
+    if technicals["Close"] > technicals["MA50"]:
+        buy_signals += 1
+    else:
+        sell_signals += 1
+
+    if technicals["MACD"] > technicals["MACD_signal"]:
+        buy_signals += 1
+    else:
+        sell_signals += 1
+
+    # Final recommendation
+    if buy_signals > sell_signals:
         return "Buy"
-    elif data['Close'].iloc[-1] < data['MA50'].iloc[-1] and data['MACD'].iloc[-1] < data['MACD_signal'].iloc[-1]:
+    elif sell_signals > buy_signals:
         return "Sell"
     else:
         return "Hold"
 
 # Streamlit App
-st.title("Stock Recommendation App")
+st.title("Stock Health Checker")
 
 ticker = st.text_input("Enter Stock Ticker:", value="AAPL").upper()
 
-if st.button("Get Recommendation"):
+if st.button("Check Stock Health"):
     try:
-        data = get_stock_data(ticker)
+        data, stock_info = get_stock_data(ticker)
         data = compute_technical_indicators(data)
         
         st.subheader("Fundamental Indicators")
         fundamentals = {
-            "Open": data["Open"].iloc[-1],
-            "High": data["High"].iloc[-1],
-            "Low": data["Low"].iloc[-1],
-            "Close": data["Close"].iloc[-1],
-            "Volume": data["Volume"].iloc[-1],
-            "Dividends": data["Dividends"].iloc[-1],
-            "Stock Splits": data["Stock Splits"].iloc[-1],
-            "52 Week High": data["High"].max(),
-            "52 Week Low": data["Low"].min(),
-            "Market Cap": yf.Ticker(ticker).info["marketCap"]
+            "ROI": stock_info.get("returnOnInvestment", "N/A"),
+            "EBITDA": stock_info.get("ebitda", "N/A"),
+            "EPS": stock_info.get("trailingEps", "N/A"),
+            "P/E": stock_info.get("trailingPE", "N/A"),
+            "P/B": stock_info.get("priceToBook", "N/A"),
+            # ... [Add other fundamental indicators here] ...
         }
         st.write(fundamentals)
         
@@ -81,14 +79,11 @@ if st.button("Get Recommendation"):
             "RSI": data["RSI"].iloc[-1],
             "MACD": data["MACD"].iloc[-1],
             "MACD Signal": data["MACD_signal"].iloc[-1],
-            "Bollinger Upper": data["BB_upper"].iloc[-1],
-            "Bollinger Lower": data["BB_lower"].iloc[-1],
-            "Stochastic %K": data["%K"].iloc[-1],
-            "Stochastic %D": data["%D"].iloc[-1]
+            # ... [Add other technical indicators here] ...
         }
         st.write(technicals)
         
-        recommendation = get_recommendation(data)
+        recommendation = get_recommendation(fundamentals, technicals)
         st.subheader(f"Recommendation: {recommendation}")
         
     except Exception as e:
