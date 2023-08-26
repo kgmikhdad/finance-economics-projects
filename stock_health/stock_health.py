ce6d4b7b0f89719a20bdbbf81b18fe1023cf0011
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-26 18:18:29 +0530

    Update stock_health.py

--- stock_health/stock_health.py
+++ stock_health/stock_health.py
@@ -1,66 +1,51 @@
-# app.py
-import pandas_datareader as web
 import streamlit as st
-import ta
-import datetime
+import yfinance as yf
+from ta import add_all_ta_features
 
-def fetch_data(ticker_symbol):
-    # Set the end date to today and the start date to one year ago
-    end = datetime.datetime.now()
-    start = end - datetime.timedelta(days=365)
-    
-    # Fetch data
-    df = web.DataReader(ticker_symbol, 'yahoo', start, end)
-    
-    # Fundamental indicators
-    fundamentals = {
-        'Open': df['Open'].iloc[-1],
-        'High': df['High'].iloc[-1],
-        'Low': df['Low'].iloc[-1],
-        'Close': df['Close'].iloc[-1],
-        'Volume': df['Volume'].iloc[-1],
-        'Adj Close': df['Adj Close'].iloc[-1],
-        # Add other available indicators...
+# Function to fetch stock data
+def get_stock_data(ticker):
+    data = yf.Ticker(ticker)
+    return data.history(period="1y")
+
+# Function to get fundamental indicators
+def get_fundamental_indicators(data):
+    # For simplicity, we'll fetch some basic indicators. You can expand this list.
+    indicators = {
+        "Open": data["Open"].iloc[-1],
+        "High": data["High"].iloc[-1],
+        "Low": data["Low"].iloc[-1],
+        "Close": data["Close"].iloc[-1],
+        "Volume": data["Volume"].iloc[-1],
+        "Dividends": data["Dividends"].iloc[-1],
+        "Stock Splits": data["Stock Splits"].iloc[-1],
+        # Add more fundamental indicators here
     }
+    return indicators
 
-    # Technical indicators using the `ta` library
-    df['RSI'] = ta.momentum.RSIIndicator(df['Close']).rsi()
-    df['MACD'] = ta.trend.MACD(df['Close']).macd()
-    df['BB_High'] = ta.volatility.BollingerBands(df['Close']).bollinger_hband()
-    df['BB_Low'] = ta.volatility.BollingerBands(df['Close']).bollinger_lband()
-    df['Stochastic Oscillator'] = ta.momentum.StochasticOscillator(df['High'], df['Low'], df['Close']).stoch()
-    df['ATR'] = ta.volatility.AverageTrueRange(df['High'], df['Low'], df['Close']).average_true_range()
-    df['MFI'] = ta.momentum.MFIIndicator(df['High'], df['Low'], df['Close'], df['Volume']).money_flow_index()
-    df['ADX'] = ta.trend.ADXIndicator(df['High'], df['Low'], df['Close']).adx()
-    df['CCI'] = ta.trend.CCIIndicator(df['High'], df['Low'], df['Close']).cci()
-    df['PPO'] = ta.momentum.PercentagePriceOscillator(df['Close']).ppo()
-
-    technicals = {
-        'RSI': df['RSI'].iloc[-1],
-        'MACD': df['MACD'].iloc[-1],
-        'Bollinger Band High': df['BB_High'].iloc[-1],
-        'Bollinger Band Low': df['BB_Low'].iloc[-1],
-        'Stochastic Oscillator': df['Stochastic Oscillator'].iloc[-1],
-        'Average True Range (ATR)': df['ATR'].iloc[-1],
-        'Money Flow Index (MFI)': df['MFI'].iloc[-1],
-        'ADX': df['ADX'].iloc[-1],
-        'Commodity Channel Index (CCI)': df['CCI'].iloc[-1],
-        'Percentage Price Oscillator (PPO)': df['PPO'].iloc[-1]
+# Function to get technical indicators
+def get_technical_indicators(data):
+    data = add_all_ta_features(data, open="Open", high="High", low="Low", close="Close", volume="Volume")
+    # For simplicity, we'll fetch some basic technical indicators. You can expand this list.
+    indicators = {
+        "RSI": data["momentum_rsi"].iloc[-1],
+        "MACD": data["trend_macd"].iloc[-1],
+        "MACD Signal": data["trend_macd_signal"].iloc[-1],
+        # Add more technical indicators here
     }
-    
-    return fundamentals, technicals
+    return indicators
 
-st.title('Stock Indicators App')
+# Streamlit App
+st.title("Stock Indicators App")
 
-ticker_symbol = st.text_input('Enter Ticker Symbol:', value='AAPL').upper()
+ticker = st.text_input("Enter Stock Ticker:", value="AAPL").upper()
 
-if st.button('Fetch Data'):
-    fundamentals, technicals = fetch_data(ticker_symbol)
-    st.write('## Fundamental Indicators')
-    st.write(fundamentals)
-    
-    st.write('## Technical Indicators')
-    st.write(technicals)
+if st.button("Get Indicators"):
+    try:
+        data = get_stock_data(ticker)
+        st.subheader("Fundamental Indicators")
+        st.write(get_fundamental_indicators(data))
+        st.subheader("Technical Indicators")
+        st.write(get_technical_indicators(data))
+    except:
+        st.write("Error fetching data. Please check the stock ticker and try again.")
 
-# For running the app, in the terminal use:
-# streamlit run app.py
