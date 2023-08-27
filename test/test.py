3dfafb6c99d27c05c8c10e472659a639f40f1a2a
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-27 19:30:02 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 def tradingview_widget(stock_symbol):
     html_code = f"""
-    <div class="tradingview-widget-container" style="width: 100%; height: 100%;">
-      <div id="tradingview_da31d" style="width: 100%; height: 100%;"></div>
+    <div class="tradingview-widget-container">
+      <div id="tradingview_9573c" style="width: 100%; height: 100%;"></div>
       <div class="tradingview-widget-copyright">
         <a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank">
           <span class="blue-text">Track all markets on TradingView</span>
@@ -22,18 +22,18 @@ def tradingview_widget(stock_symbol):
       "locale": "in",
       "enable_publishing": false,
       "allow_symbol_change": true,
-      "container_id": "tradingview_da31d"
+      "container_id": "tradingview_9573c"
       }}
       );
       </script>
     </div>
     """
-    st.components.v1.html(html_code, height=10000, width=1000)
+    st.components.v1.html(html_code, height=2000, width=2000)
 
 st.title("TradingView Widget in Streamlit")
 
 # User input for stock symbol
-stock_symbol = st.text_input("Enter Stock Symbol (e.g. NASDAQ:AAPL):", "NASDAQ:AAPL").upper()
+stock_symbol = st.text_input("Enter Stock Symbol (e.g. NSE:SBIN):", "NSE:SBIN").upper()
 
 if stock_symbol:
     tradingview_widget(stock_symbol)
