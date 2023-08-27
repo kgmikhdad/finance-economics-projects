c5115d3bddda90779892d19c511ad9a291a496c8
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-27 19:23:25 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-def tradingview_widget():
-    html_code = """
+def tradingview_widget(stock_symbol):
+    html_code = f"""
     <div class="tradingview-widget-container" style="width: 100%; height: 100%;">
       <div id="tradingview_da31d" style="width: 100%; height: 100%;"></div>
       <div class="tradingview-widget-copyright">
@@ -12,9 +12,9 @@ def tradingview_widget():
       <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
       <script type="text/javascript">
       new TradingView.widget(
-      {
+      {{
       "autosize": true,
-      "symbol": "NASDAQ:AAPL",
+      "symbol": "{stock_symbol}",
       "interval": "D",
       "timezone": "Etc/UTC",
       "theme": "light",
@@ -23,12 +23,17 @@ def tradingview_widget():
       "enable_publishing": false,
       "allow_symbol_change": true,
       "container_id": "tradingview_da31d"
-      }
+      }}
       );
       </script>
     </div>
     """
-    st.components.v1.html(html_code)
+    st.components.v1.html(html_code, height=800, width=1200)
 
 st.title("TradingView Widget in Streamlit")
-tradingview_widget()
+
+# User input for stock symbol
+stock_symbol = st.text_input("Enter Stock Symbol (e.g. NASDAQ:AAPL):", "NASDAQ:AAPL").upper()
+
+if stock_symbol:
+    tradingview_widget(stock_symbol)
