ecbf331e1c020d0b3b6067fefdb612b8abd6305d
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-27 19:16:40 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,42 +1,34 @@
 import streamlit as st
 
-def main():
-    st.title("My Portfolio Dashboard")
+def tradingview_widget():
+    html_code = """
+    <div class="tradingview-widget-container">
+      <div id="tradingview_da31d"></div>
+      <div class="tradingview-widget-copyright">
+        <a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank">
+          <span class="blue-text">Track all markets on TradingView</span>
+        </a>
+      </div>
+      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
+      <script type="text/javascript">
+      new TradingView.widget(
+      {
+      "autosize": true,
+      "symbol": "NASDAQ:AAPL",
+      "interval": "D",
+      "timezone": "Etc/UTC",
+      "theme": "light",
+      "style": "1",
+      "locale": "in",
+      "enable_publishing": false,
+      "allow_symbol_change": true,
+      "container_id": "tradingview_da31d"
+      }
+      );
+      </script>
+    </div>
+    """
+    st.components.v1.html(html_code, height=600)
 
-    # Introduction
-    st.header("Introduction")
-    st.write("""
-    Hello! I'm [Your Name], a passionate developer/data scientist with experience in [Your Skills].
-    This is a brief overview of my projects and experiences.
-    """)
-
-    # Projects
-    st.header("Projects")
-    projects = {
-        "Project 1": {
-            "description": "This project is about ...",
-            "link": "https://github.com/yourusername/project1"
-        },
-        "Project 2": {
-            "description": "This project focuses on ...",
-            "link": "https://github.com/yourusername/project2"
-        },
-        # Add more projects as needed
-    }
-
-    for project_name, project_info in projects.items():
-        st.subheader(project_name)
-        st.write(project_info["description"])
-        st.write(f"[Link to the project]({project_info['link']})")
-
-    # Contact
-    st.header("Contact")
-    st.write("""
-    Feel free to reach out to me!
-    - Email: [youremail@example.com](mailto:youremail@example.com)
-    - LinkedIn: [Your LinkedIn Profile](https://www.linkedin.com/in/yourusername/)
-    - GitHub: [Your GitHub Profile](https://github.com/yourusername)
-    """)
-
-if __name__ == "__main__":
-    main()
+st.title("TradingView Widget in Streamlit")
+tradingview_widget()
