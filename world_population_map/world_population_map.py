b535c923e8032b39f0431de8e4b5eb37af1ce02a
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-27 12:31:38 +0530

    Update world_population_map.py

--- world_population_map/world_population_map.py
+++ world_population_map/world_population_map.py
@@ -1,1 +1,28 @@
+import streamlit as st
+import folium
+import geopandas as gpd
+from streamlit_folium import folium_static
 
+# Load the world map data
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+# Create a map object
+m = folium.Map(location=[20,0], zoom_start=2)
+
+# Add the data to the map
+folium.Choropleth(
+    geo_data=world,
+    name='choropleth',
+    data=world,
+    columns=['name', 'pop_est'],
+    key_on='feature.properties.name',
+    fill_color='YlGn',
+    fill_opacity=0.7,
+    line_opacity=0.2,
+    legend_name='Population by Country'
+).add_to(m)
+
+folium.LayerControl().add_to(m)
+
+# Display the map in Streamlit
+folium_static(m)
