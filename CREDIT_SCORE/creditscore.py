bf9cd65d442de8eacf934347344ba575bd9ab244
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-28 11:02:48 +0530

    Update creditscore.py

--- CREDIT_SCORE/creditscore.py
+++ CREDIT_SCORE/creditscore.py
@@ -16,11 +16,11 @@ def get_credit_score_category(score):
     if 800 <= score <= 900:
         return "<div style='background-color: green; padding: 10px; text-align: center; border-radius: 5px;'>Exceptional</div>"
     elif 740 <= score <= 799:
-        return "<div style='background-color: #ADFF2F; padding: 10px; text-align: center; border-radius: 5px;'>Very Good</div>"  # GreenYellow
+        return "<div style='background-color: #ADFF2F; padding: 10px; text-align: center; border-radius: 5px;'>Very Good</div>"  
     elif 670 <= score <= 739:
         return "<div style='background-color: yellow; padding: 10px; text-align: center; border-radius: 5px;'>Good</div>"
     elif 580 <= score <= 669:
-        return "<div style='background-color: #FFD700; padding: 10px; text-align: center; border-radius: 5px;'>Fair</div>"  # Goldenrod
+        return "<div style='background-color: #FFD700; padding: 10px; text-align: center; border-radius: 5px;'>Fair</div>"  
     else:
         return "<div style='background-color: red; padding: 10px; text-align: center; border-radius: 5px;'>Poor</div>"
 
@@ -40,16 +40,13 @@ def calculate_credit_score():
     st.title("Credit Score Calculator")
     st.write("Enter the following details to calculate your Credit score:")
 
-    # Get user input with specified ranges
     payment_history = get_input("Percentage of timely payments (0-100): ", 0, 100)
     credit_utilization = get_input("Credit utilization ratio (0-100): ", 0, 100)
     length_of_credit_history = get_input("Length of credit history in years (e.g., 0-50): ", 0, 50)
     types_of_credit = get_input("Number of types of credit (e.g., 1-5): ", 1, 5)
     new_credit = get_input("Number of new credits opened recently (e.g., 0-10): ", 0, 10)
 
-    # Display the "Enter" button
     if st.button('Enter'):
-        # Calculate score
         score = 0
         score += payment_history * 3.5
         score += (100 - credit_utilization) * 3.0
@@ -57,17 +54,13 @@ def calculate_credit_score():
         score += types_of_credit * 20
         score -= new_credit * 20
 
-        # Ensure score boundaries
         score = max(300, min(900, score))
 
         st.markdown(f"<h2 style='text-align: center; font-family: Times New Roman, Times, serif;'>Your estimated Credit score is: {score}</h2>", unsafe_allow_html=True)
         
-        # Get credit score category
         category_html = get_credit_score_category(score)
         st.markdown(category_html, unsafe_allow_html=True)
-        # ... [rest of your code]
 
-    # Dropdown for credit score improvement tips
     print()
     print()
     if st.button("How to improve your credit score"):
