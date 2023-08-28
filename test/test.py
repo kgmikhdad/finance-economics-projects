55ad8091d790adc1ecb9f81d355f3f2291619b89
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-28 10:43:14 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,39 +1,34 @@
 import streamlit as st
+import yfinance as yf
+import plotly.graph_objects as go
 
-def tradingview_widget(stock_symbol):
-    html_code = f"""
-    <div class="tradingview-widget-container">
-      <div id="tradingview_9573c" style="width: 100%; height: 100%;"></div>
-      <div class="tradingview-widget-copyright">
-        <a href="https://in.tradingview.com/" rel="noopener nofollow" target="_blank">
-          <span class="blue-text">Track all markets on TradingView</span>
-        </a>
-      </div>
-      <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-      <script type="text/javascript">
-      new TradingView.widget(
-      {{
-      "autosize": true,
-      "symbol": "{stock_symbol}",
-      "interval": "D",
-      "timezone": "Etc/UTC",
-      "theme": "light",
-      "style": "1",
-      "locale": "in",
-      "enable_publishing": false,
-      "allow_symbol_change": true,
-      "container_id": "tradingview_9573c"
-      }}
-      );
-      </script>
-    </div>
-    """
-    st.components.v1.html(html_code, height=2000, width=2000)
+def plot_stock_chart(stock_name):
+    # Fetch data
+    stock = yf.Ticker(stock_name)
+    hist_data = stock.history(period='1y')
+    
+    # Check if data was fetched successfully
+    if hist_data.empty:
+        st.write(f"No data available for {stock_name}. Please enter a valid stock ticker.")
+        return
+    
+    # Plotting the stock data
+    fig = go.Figure(data=[go.Candlestick(x=hist_data.index,
+                                         open=hist_data['Open'],
+                                         high=hist_data['High'],
+                                         low=hist_data['Low'],
+                                         close=hist_data['Close'])])
+    
+    fig.update_layout(title=f'{stock_name} Stock Price Over the Last Year',
+                      xaxis_title='Date',
+                      yaxis_title='Stock Price',
+                      xaxis_rangeslider_visible=False)
+    
+    st.plotly_chart(fig)
 
-st.title("TradingView Widget in Streamlit")
+# Streamlit UI
+st.title('Stock Price Chart App')
+stock_name = st.text_input('Enter the stock name (e.g. AAPL for Apple): ')
 
-# User input for stock symbol
-stock_symbol = st.text_input("Enter Stock Symbol (e.g. NSE:SBIN):", "NSE:SBIN").upper()
-
-if stock_symbol:
-    tradingview_widget(stock_symbol)
+if stock_name:
+    plot_stock_chart(stock_name)
