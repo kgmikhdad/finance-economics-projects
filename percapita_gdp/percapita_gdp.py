6157d8004032ca851865507a16168534e1b1d158
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-28 11:03:36 +0530

    Update percapita_gdp.py

--- percapita_gdp/percapita_gdp.py
+++ percapita_gdp/percapita_gdp.py
@@ -3,19 +3,14 @@ import folium
 import geopandas as gpd
 from streamlit_folium import folium_static
 
-# Set the layout of the Streamlit app to wide mode
 st.set_page_config(layout="wide")
 
-# Load the world map data
 world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
 
-# Calculate GDP per capita
 world['gdp_per_capita'] = world['gdp_md_est'] / world['pop_est']
 
-# Create a map object with a larger width and height
 m = folium.Map(location=[20,0], zoom_start=2, width='100%', height='100%')
 
-# Add the data to the map
 folium.Choropleth(
     geo_data=world,
     name='choropleth',
@@ -30,6 +25,5 @@ folium.Choropleth(
 
 folium.LayerControl().add_to(m)
 
-# Display the map in Streamlit
 folium_static(m)
 
