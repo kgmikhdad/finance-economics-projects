d1b42faa4e87baee4ab4bcd9e06b5c5f12206091
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-08-28 11:03:13 +0530

    Update ROI.py

--- ROI_Calculator/ROI.py
+++ ROI_Calculator/ROI.py
@@ -1,13 +1,10 @@
 import streamlit as st
 
-# Function to calculate ROI
 def calculate_roi(interest_rate, amount, duration):
     total_amount = amount * ((1 + (interest_rate / 100)) ** duration)
     return total_amount
 
-# Streamlit app
 def main():
-    # Custom CSS for centering the title and setting the font style
     def custom_css():
         st.markdown("""
             <style>
@@ -23,12 +20,10 @@ def main():
     custom_css()
     st.title("ROI Calculator")
 
-    # User input
     interest_rate = st.slider("Interest Rate (%)", 0.0, 20.0, 5.0, 0.1)
     amount = st.number_input("Amount (₹)", min_value=100.0, max_value=10000000.0, value=1000.0, step=50.0)
     duration = st.slider("Duration (years)", 1, 40, 1)
 
-    # Calculate ROI
     if st.button("Calculate ROI"):
         roi = calculate_roi(interest_rate, amount, duration)
         st.write(f"Total amount after {duration} years: ₹{roi:.2f}")
