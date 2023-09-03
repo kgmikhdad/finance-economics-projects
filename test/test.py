bfb235ce34b60e2e3b28428d710208a196fffdd4
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-09-03 23:52:41 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,3 +1,5 @@
+import streamlit as st
+
 def calculate_roi(interest_rate, amount, duration, frequency):
     if frequency == "Per Day":
         total_days = duration * 365
@@ -19,9 +21,48 @@ def calculate_roi(interest_rate, amount, duration, frequency):
 def adjust_for_inflation(amount, inflation_rate, duration):
     return amount / ((1 + inflation_rate / 100) ** duration)
 
-# ... [rest of the code remains the same]
+def main():
+    def custom_css():
+        st.markdown("""
+            <style>
+                h1 {
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    font-family: 'Times New Roman', Times, serif;
+                }
+                .stApp > div > div > div > div {
+                    align-items: center;
+                }
+            </style>
+        """, unsafe_allow_html=True)
 
-if st.button("Calculate ROI"):
-    roi = calculate_roi(interest_rate, amount, duration, frequency)
-    roi_adjusted = adjust_for_inflation(roi, inflation_rate, duration)
-    st.write(f"Investing in {selected_instrument}, the total amount after {duration} years with interest calculated {frequency.lower()} (adjusted for inflation) will be: ₹{roi_adjusted:.2f}")
+    custom_css()
+    st.title("ROI Calculator")
+
+    financial_instruments = [
+        "Bonds", "Certificates of Deposit (CDs)", "Stocks", "Fixed Annuities", 
+        "Money Market Instruments", "Treasury Bills", "Commercial Paper", 
+        "Bankers' Acceptances", "Mortgage-Backed Securities (MBS)", 
+        "Collateralized Debt Obligations (CDOs)", "Fixed Rate Loan Notes"
+    ]
+    
+    selected_instrument = st.selectbox("Select Financial Instrument", financial_instruments)
+    interest_rate = st.slider("Interest Rate (%)", 0.0, 20.0, 5.0, 0.1)
+    inflation_rate = st.slider("Inflation Rate (%)", 0.0, 10.0, 2.0, 0.1)
+    amount = st.number_input("Amount (₹)", min_value=100.0, max_value=10000000.0, value=1000.0, step=50.0)
+    duration = st.slider("Duration (years)", 1, 40, 1)
+    frequency = st.selectbox("Interest Return Frequency", ["Per Day", "Per Month", "Per Three Months", "Per Six Months", "Per Year"])
+
+    if st.button("Calculate ROI"):
+        roi = calculate_roi(interest_rate, amount, duration, frequency)
+        roi_adjusted = adjust_for_inflation(roi, inflation_rate, duration)
+        st.write(f"Investing in {selected_instrument}, the total amount after {duration} years with interest calculated {frequency.lower()} (adjusted for inflation) will be: ₹{roi_adjusted:.2f}")
+        
+        # Graphical representation of ROI over time
+        years = list(range(1, duration + 1))
+        roi_values = [adjust_for_inflation(calculate_roi(interest_rate, amount, year, frequency), inflation_rate, year) for year in years]
+        st.line_chart({"ROI": roi_values}, use_container_width=True)
+
+if __name__ == "__main__":
+    main()
