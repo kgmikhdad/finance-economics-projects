dd05181d98ed15272b68819fb10a0dba7f1af4de
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-09-03 21:56:08 +0530

    Update ROI.py

--- ROI_Calculator/ROI.py
+++ ROI_Calculator/ROI.py
@@ -19,7 +19,6 @@ def calculate_roi(interest_rate, amount, duration, frequency):
     return total_amount
 
 def main():
-def main():
     def custom_css():
         st.markdown("""
             <style>
@@ -37,6 +36,7 @@ def main():
 
     custom_css()
     st.title("ROI Calculator")
+
     financial_instruments = [
         "Bonds", "Certificates of Deposit (CDs)", "Stocks", "Fixed Annuities", 
         "Money Market Instruments", "Treasury Bills", "Commercial Paper", 
