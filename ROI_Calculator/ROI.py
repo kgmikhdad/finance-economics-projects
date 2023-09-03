131f3c4761bddb1c281566d753be6200e3812f13
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-09-03 21:32:07 +0530

    Update ROI.py

--- ROI_Calculator/ROI.py
+++ ROI_Calculator/ROI.py
@@ -34,6 +34,14 @@ def main():
     custom_css()
     st.title("ROI Calculator")
 
+    financial_instruments = [
+        "Bonds", "Certificates of Deposit (CDs)", "Stocks", "Fixed Annuities", 
+        "Money Market Instruments", "Treasury Bills", "Commercial Paper", 
+        "Bankers' Acceptances", "Mortgage-Backed Securities (MBS)", 
+        "Collateralized Debt Obligations (CDOs)", "Fixed Rate Loan Notes"
+    ]
+    
+    selected_instrument = st.selectbox("Select Financial Instrument", financial_instruments)
     interest_rate = st.slider("Interest Rate (%)", 0.0, 20.0, 5.0, 0.1)
     amount = st.number_input("Amount (₹)", min_value=100.0, max_value=10000000.0, value=1000.0, step=50.0)
     duration = st.slider("Duration (years)", 1, 40, 1)
@@ -41,7 +49,7 @@ def main():
 
     if st.button("Calculate ROI"):
         roi = calculate_roi(interest_rate, amount, duration, frequency)
-        st.write(f"Total amount after {duration} years with interest calculated {frequency.lower()}: ₹{roi:.2f}")
+        st.write(f"Investing in {selected_instrument}, the total amount after {duration} years with interest calculated {frequency.lower()} will be: ₹{roi:.2f}")
 
 if __name__ == "__main__":
     main()
