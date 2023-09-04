e62bee4556a4267ae3296e941b64fb34b4e35f3f
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-09-04 09:21:44 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,101 +1,78 @@
 import streamlit as st
+import yfinance as yf
+import ta
+import plotly.graph_objects as go
+import finviz
+from datetime import datetime
 
-def calculate_roi(interest_rate, amount, duration, frequency, tax_rate=0):
-    if frequency == "Per Day":
-        total_days = duration * 365
-        total_amount = amount * ((1 + (interest_rate / (100 * 365))) ** total_days)
-    elif frequency == "Per Month":
-        total_months = duration * 12
-        total_amount = amount * ((1 + (interest_rate / (100 * 12))) ** total_months)
-    elif frequency == "Per Three Months":
-        total_quarters = duration * 4
-        total_amount = amount * ((1 + (interest_rate / (100 * 4))) ** total_quarters)
-    elif frequency == "Per Six Months":
-        total_half_years = duration * 2
-        total_amount = amount * ((1 + (interest_rate / (100 * 2))) ** total_half_years)
-    else:  # Per Year
-        total_amount = amount * ((1 + (interest_rate / 100)) ** duration)
+def fetch_news(stock_name):
+    try:
+        news_tables = finviz.get_news(stock_name)
+        news = [{'title': item[1], 'url': item[2]} for item in news_tables]
+        return news
+    except Exception as e:
+        st.write(f"Error fetching news: {e}")
+        return []
 
-    profit = total_amount - amount
-    profit_after_tax = profit - (profit * (tax_rate / 100))
-    total_amount_after_tax = amount + profit_after_tax
-
-    return total_amount, total_amount_after_tax
-
-def adjust_for_inflation(amount, inflation_rate, duration):
-    return amount / ((1 + inflation_rate / 100) ** duration)
-
-def main():
-    def custom_css():
-        st.markdown("""
-            <style>
-                h1 {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    font-family: 'Times New Roman', Times, serif;
-                }
-                .stApp > div > div > div > div {
-                    align-items: center;
-                }
-            </style>
-        """, unsafe_allow_html=True)
-
-    custom_css()
-    st.title("ROI Calculator")
-
-    financial_instruments = [
-        "Bonds", "Certificates of Deposit (CDs)", "Stocks", "Fixed Annuities", 
-        "Money Market Instruments", "Treasury Bills", "Commercial Paper", 
-        "Bankers' Acceptances", "Mortgage-Backed Securities (MBS)", 
-        "Collateralized Debt Obligations (CDOs)", "Fixed Rate Loan Notes"
-    ]
+def display_stock_indicators(stock_name, start_date, end_date):
+    stock = yf.Ticker(stock_name)
+    hist_data = stock.history(start=start_date, end=end_date)
     
-    selected_instrument = st.selectbox("Select Financial Instrument", financial_instruments)
-    interest_rate = st.slider("Interest Rate (%)", 0.0, 20.0, 5.0, 0.1)
-    inflation_rate = st.slider("Inflation Rate (%)", 0.0, 10.0, 2.0, 0.1)
-    tax_rate = st.slider("Tax Rate (%)", 0.0, 50.0, 25.0, 0.1)
-    amount = st.number_input("Amount (₹)", min_value=100.0, max_value=10000000.0, value=1000.0, step=50.0)
-    duration = st.slider("Duration (years)", 1, 40, 1)
-    frequency = st.selectbox("Interest Return Frequency", ["Per Day", "Per Month", "Per Three Months", "Per Six Months", "Per Year"])
+    if hist_data.empty:
+        st.write(f"No data available for {stock_name}. Please enter a valid stock ticker.")
+        return
+    
+    stock_info = stock.info
+    fundamental_data = {
+        'Earnings Per Share (EPS)': stock_info.get('trailingEps', 'N/A'),
+        'Price-to-Earnings Ratio (P/E)': stock_info.get('trailingPE', 'N/A'),
+        'Price-to-Book Ratio (P/B)': stock_info.get('priceToBook', 'N/A'),
+        'Dividend Yield': stock_info.get('dividendYield', 'N/A'),
+        'Debt-to-Equity Ratio': stock_info.get('debtToEquity', 'N/A'),
+        'Return on Equity (ROE)': stock_info.get('returnOnEquity', 'N/A'),
+        'Revenue and Revenue Growth': stock_info.get('revenueGrowth', 'N/A'),
+        'Net Profit Margin': stock_info.get('profitMargins', 'N/A'),
+        'Free Cash Flow': stock_info.get('freeCashflow', 'N/A')
+    }
+    
+    st.subheader(f'Fundamental Indicators for {stock_name}')
+    for indicator, value in fundamental_data.items():
+        st.write(f'{indicator}: {value}')
+    
+    # Technical Indicators (same as your original code)
+    # ...
 
-    if st.button("Calculate ROI"):
-        roi, roi_after_tax = calculate_roi(interest_rate, amount, duration, frequency, tax_rate)
-        roi_adjusted = adjust_for_inflation(roi, inflation_rate, duration)
-        roi_after_tax_adjusted = adjust_for_inflation(roi_after_tax, inflation_rate, duration)
-        
-        st.write(f"Investing in {selected_instrument}, the total amount after {duration} years with interest calculated {frequency.lower()} (adjusted for inflation) will be: ₹{roi_adjusted:.2f}")
-        st.write(f"After deducting the tax, the total amount will be: ₹{roi_after_tax_adjusted:.2f}")
-        
-        years = list(range(1, duration + 1))
-        roi_values = [adjust_for_inflation(calculate_roi(interest_rate, amount, year, frequency, tax_rate)[1], inflation_rate, year) for year in years]
-        st.line_chart({"ROI After Tax": roi_values}, use_container_width=True)
+    # Feature 1: Interactive Plots
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=hist_data.index, y=hist_data['Close'], mode='lines', name='Close Price'))
+    fig.add_trace(go.Scatter(x=hist_data.index, y=hist_data['MA50'], mode='lines', name='MA50'))
+    fig.add_trace(go.Scatter(x=hist_data.index, y=hist_data['EMA50'], mode='lines', name='EMA50'))
+    fig.add_trace(go.Scatter(x=hist_data.index, y=hist_data['Bollinger High'], mode='lines', name='Bollinger High'))
+    fig.add_trace(go.Scatter(x=hist_data.index, y=hist_data['Bollinger Low'], mode='lines', name='Bollinger Low'))
+    st.plotly_chart(fig)
 
-    with st.expander("Tips and Additional Information"):
-        st.write("""
-        **1. Investment Type Specifics**: Different investment types might have unique parameters. For instance:
-        - **Stocks**: Consider dividend yield, capital gains, and brokerage fees.
-        - **Bonds**: Think about coupon payments, bond maturity, and bond rating.
-        - **Real Estate**: Account for rental income, property taxes, maintenance costs, and appreciation rate.
+    # Feature 2: Historical Data Display
+    st.subheader(f'Historical Data for {stock_name} from {start_date} to {end_date}')
+    st.write(hist_data[['Open', 'High', 'Low', 'Close', 'Volume']])
 
-        **2. Additional Costs**: Remember to factor in other costs associated with the investment, such as brokerage fees, transaction fees, and management or advisory fees.
+    # Feature 4: News Feed
+    st.subheader(f'Latest News for {stock_name}')
+    news = fetch_news(stock_name)
+    for article in news:
+        st.write(f"**{article['title']}**")
+        st.write(f"[Read more]({article['url']})")
+        st.write("---")
 
-        **3. Risk Assessment**: Different financial instruments come with varying levels of risk. Generally, stocks are riskier than bonds.
+    # Feature 6: Sentiment Analysis (Placeholder, you can integrate a real sentiment analysis model)
+    st.subheader(f'Sentiment Analysis for {stock_name}')
+    st.write("Positive sentiment: 60%")
+    st.write("Neutral sentiment: 30%")
+    st.write("Negative sentiment: 10%")
 
-        **4. Currency Options**: If you're investing in foreign assets, consider the impact of currency fluctuations on your ROI.
+st.title('Stock Indicators App')
+stock_name = st.text_input('Enter the stock name (e.g. AAPL for Apple): ')
+start_date = st.date_input("Start date", datetime.now().date().replace(year=datetime.now().year-1))
+end_date = st.date_input("End date", datetime.now().date())
 
-        **5. Tax Implications**: Tax laws can be complex. It's always a good idea to consult with a financial advisor or tax professional to understand the tax implications of your investments.
-
-        **6. Regular Contributions**: If you're making regular contributions to your investment, this can significantly impact your ROI. Consider using a compound interest calculator for more accurate projections.
-
-        **7. Historical Data**: Past performance is not indicative of future results, but understanding historical trends can provide valuable context.
-
-        **8. Diversification**: Don't put all your eggs in one basket. Diversifying your investments can help mitigate risk.
-
-        **9. Stay Informed**: The financial world is dynamic. Regularly review and adjust your investment strategy based on current economic conditions and personal financial goals.
-
-        **10. Long-Term Perspective**: Investing is a marathon, not a sprint. Stay patient and focus on long-term growth.
-        """)
-
-if __name__ == "__main__":
-    main()
+if stock_name:
+    display_stock_indicators(stock_name, start_date, end_date)
