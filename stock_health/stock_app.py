4f7b0e050060fedeb20d396154dfc290ac267772
Muhammed Mikhdad K G <96974190+kgmikhdad@users.noreply.github.com>
2023-09-14 21:54:22 +0530

    Update stock_app.py

--- stock_health/stock_app.py
+++ stock_health/stock_app.py
@@ -2,16 +2,11 @@ import streamlit as st
 import yfinance as yf
 import ta
 
-def display_stock_indicators(stock_name):
-    with st.spinner(f"Fetching data for {stock_name}..."):
-        stock = yf.Ticker(stock_name)
-        hist_data = stock.history(period='1y')
-    
-    if hist_data.empty:
-        st.error(f"No data available for {stock_name}. Please enter a valid stock ticker.")
-        return
-    
-    stock_info = stock.info
+def fetch_stock_data(stock_name):
+    stock = yf.Ticker(stock_name)
+    return stock.history(period='1y'), stock.info
+
+def display_fundamental_data(stock_info):
     fundamental_data = {
         'Earnings Per Share (EPS)': stock_info.get('trailingEps', 'N/A'),
         'Price-to-Earnings Ratio (P/E)': stock_info.get('trailingPE', 'N/A'),
@@ -24,11 +19,11 @@ def display_stock_indicators(stock_name):
         'Free Cash Flow': stock_info.get('freeCashflow', 'N/A')
     }
     
-    st.subheader(f'Fundamental Indicators for {stock_name}')
+    st.subheader('Fundamental Indicators')
     for indicator, value in fundamental_data.items():
         st.write(f'{indicator}: {value}')
-    
-    # Technical Indicators
+
+def display_technical_indicators(hist_data):
     hist_data['MA50'] = hist_data['Close'].rolling(window=50).mean()
     hist_data['EMA50'] = hist_data['Close'].ewm(span=50, adjust=False).mean()
     macd = ta.trend.MACD(hist_data['Close'])
@@ -45,18 +40,29 @@ def display_stock_indicators(stock_name):
     parabolic_sar = ta.trend.PSARIndicator(hist_data['High'], hist_data['Low'], hist_data['Close'])
     hist_data['Parabolic SAR'] = parabolic_sar.psar()
 
-    st.subheader(f'Technical Indicators for {stock_name}')
+    st.subheader('Technical Indicators')
     st.write(f'Moving Average (MA): {hist_data["MA50"].iloc[-1]}')
     st.write(f'Exponential Moving Average (EMA): {hist_data["EMA50"].iloc[-1]}')
-    st.write(f'Moving Average Convergence Divergence (MACD): {hist_data["MACD"].iloc[-1]}')
-    st.write(f'Relative Strength Index (RSI): {hist_data["RSI"].iloc[-1]}')
+    st.write(f'MACD: {hist_data["MACD"].iloc[-1]}')
+    st.write(f'RSI: {hist_data["RSI"].iloc[-1]}')
     st.write(f'Bollinger Bands: {hist_data["Bollinger High"].iloc[-1]} (High), {hist_data["Bollinger Low"].iloc[-1]} (Low)')
     st.write(f'Stochastic Oscillator: {hist_data["Stochastic Oscillator"].iloc[-1]}')
-    st.write(f'Average True Range (ATR): {hist_data["ATR"].iloc[-1]}')
-    st.write(f'Parabolic SAR (Stop and Reverse): {hist_data["Parabolic SAR"].iloc[-1]}')
+    st.write(f'ATR: {hist_data["ATR"].iloc[-1]}')
+    st.write(f'Parabolic SAR: {hist_data["Parabolic SAR"].iloc[-1]}')
 
-st.title('Stock Indicators App')
-stock_name = st.text_input('Enter the stock name (e.g. AAPL for Apple): ')
+def main():
+    st.title('Stock Indicators App')
+    stock_name = st.text_input('Enter the stock name (e.g. AAPL for Apple): ')
 
-if stock_name:
-    display_stock_indicators(stock_name)
+    if stock_name:
+        hist_data, stock_info = fetch_stock_data(stock_name)
+        
+        if hist_data.empty:
+            st.error(f"No data available for {stock_name}. Please enter a valid stock ticker.")
+            return
+        
+        display_fundamental_data(stock_info)
+        display_technical_indicators(hist_data)
+
+if __name__ == "__main__":
+    main()
